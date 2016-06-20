6c0afae1acec1c9df63855f0399a30d00a57bf14
nguyentdung <nguyentdung@dena-travel.com>
2016-06-20 20:53:17 +0900

    update setting file

--- fraby/settings.py
+++ fraby/settings.py
@@ -121,4 +121,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_ROOT, '/var/www/static/']
+STATICFILES_DIRS = ['/var/www/static/']
