5c16abcae6003ab50bc003443b3a087435e0473c
nguyentdung <nguyentdung@dena-travel.com>
2016-07-01 16:27:36 +0900

    using template

--- music/views.py
+++ music/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from .models import Album
+
 
 def index(request):
-    return HttpResponse('<h1>Music Page Workerd</h1>')
+    all_albums = Album.objects.all()
+    context = {'all_albums': all_albums}
+    return render(request=request, template_name='music/index.html', context=context)
+
+
+def detail(request, album_id):
+    return HttpResponse('<h2>Details for Album id: {0}</h2>'.format(album_id))
