a46d7a0bba9f6d8f38d4cb0f94f431bce51607df
nguyentdung <nguyentdung@dena-travel.com>
2016-07-01 19:03:26 +0900

    use get_object_or_404 shortcuts and register url namespance by app_name

--- music/views.py
+++ music/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 
@@ -11,8 +9,5 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404('Album does not exist')
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
