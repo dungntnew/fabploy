a11c9cde312872d16361cf5b7874e02660f4848e
nguyentdung <nguyentdung@dena-travel.com>
2016-07-01 18:20:55 +0900

    use url for create link

--- music/urls.py
+++ music/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     # /music/71/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='album detail'),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
