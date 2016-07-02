469ed033dd61750473943b68c78d85f2fe4a59a9
Dung Nguyen <dungntnew@gmail.com>
2016-07-02 20:19:48 +0900

    using generic views

--- music/views.py
+++ music/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render, get_object_or_404
+
+#
+#
+# def index(request):
+#     all_albums = Album.objects.all()
+#     context = {'all_albums': all_albums}
+#     return render(request=request, template_name='music/index.html', context=context)
+#
+#
+# def detail(request, album_id):
+#     album = get_object_or_404(Album, pk=album_id)
+#     return render(request, 'music/detail.html', {'album': album})
+#
+#
+
+
+from django.views import generic
 from .models import Album, Song
 
 
-def index(request):
-    all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request=request, template_name='music/index.html', context=context)
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album})
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
 
 
 def favorite(request, album_id):
