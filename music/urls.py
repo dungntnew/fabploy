354e01691959e7113743f5996f7a347c6e3598e3
Dung Nguyen <dungntnew@gmail.com>
2016-07-03 12:41:35 +0900

    register user

--- music/urls.py
+++ music/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     # /music/
     url(r'^$', views.IndexView.as_view(), name='index'),
 
+    # /music/register
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
     # /music/71/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
--- music/views.py
+++ music/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+
+from .forms import UserForm
 
 #
 #
@@ -62,3 +66,34 @@ def favorite(request, album_id):
         selected_song.is_favorite = True
         selected_song.save()
         return render(request, 'music/detail.html', {'album': album})
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('music:index')
+
+        return render(request, self.template_name, {'form': form})
